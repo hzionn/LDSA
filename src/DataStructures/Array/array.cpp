d5941f0249a0c1fb289285c3034627eef41f0e91
hzionn <zionn98tiger@gmail.com>
2024-05-14 01:47:24 +0800

    add more comments

--- src/DataStructures/Array/array.cpp
+++ src/DataStructures/Array/array.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int main() {
+    // init a array with size 5
     int myArray[5] = {10, 20, 30, 40, 50};
 
+    // print the array
     for (int i = 0; i < 5; i++) {
         cout << myArray[i] << endl;
     }
 
+    // init a str with size 6
     char strArray[6] = "hello";
 
     for (int i = 0; i < 6; i++) {
