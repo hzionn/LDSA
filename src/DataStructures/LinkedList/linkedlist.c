525ef2e7601c1dda0ed6e25cbbde957aa3b24e6a
hzionn <zionn98tiger@gmail.com>
2024-05-19 22:37:21 +0800

    add linked list data structure in c

--- src/DataStructures/LinkedList/linkedlist.c
+++ src/DataStructures/LinkedList/linkedlist.c
@@ -1,5 +1,64 @@
-#include <stdio.h>
+// linked list
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning
+void insertAtBeginning(struct Node** headRef, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *headRef;
+    *headRef = newNode;
+}
+
+// Function to insert a node at the end
+void insertAtEnd(struct Node** headRef, int data) {
+    struct Node* newNode = createNode(data);
+    if (*headRef == NULL) {
+        *headRef = newNode;
+        return;
+    }
+    struct Node* temp = *headRef;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Main function to demonstrate linked list operations
 int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtBeginning(&head, 5);
+
+    printf("Linked List: ");
+    printList(head);
+
     return 0;
 }
