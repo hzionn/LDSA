aaaf9f434d4b2f722bdedd3163b0114ff950e460
hzionn <zionn98tiger@gmail.com>
2024-05-22 23:20:40 +0800

    add unfinish merge sort

---
+++ src/Algorithms/Sorting/merge_sort.c
@@ -0,0 +1,14 @@
+/*
+ * merge sort 
+ */
+
+// write me merge sort algorithm
+
+#include <stdio.h>
+#include "helper.h"
+
+void merge_sort(int arr[], int size) {}
+
+int main(void) {
+    return 0;
+}
